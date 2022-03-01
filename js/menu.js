3890bd8b55a7c44bd40d48636822f9491ce43cdc
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-03-01 20:09:51 +0200

    the pop-up menu

--- js/menu.js
+++ js/menu.js
@@ -7,8 +7,36 @@ btn.addEventListener('click', () => {
   document.body.classList.toggle('no-scroll');
 });
 
+// the pop-up menu
+
 menuList.forEach((list) => {
   list.addEventListener('click', () => {
     menu.classList.remove('active');
   });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var bt = document.getElementsByClassName("open");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+bt.onclick = function() {
+  alert("yes ");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
