2ddb081da512040dcb5136d5ba153ea5ab56a9ac
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-03-03 00:22:47 +0200

    My pop-up first commit

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,51 @@
+import workData from './index.js';
+
+const workOuterContainer = document.querySelector('.works');
+
+// create works
+function generateWorks({ name, tech, ImgUrl }, index) {
+  const workContainer = document.createElement('div');
+  workContainer.classList.add('works-item');
+
+  workContainer.innerHTML = `
+  <div class="work-img">
+    <div class="proj-image">
+      <img src="Images/${ImgUrl}" alt="${name}" />
+    </div>
+  </div>
+  <div class="lower">
+    <h3>${name}</h3>
+    <ul class="tags">
+    ${tech.map(
+    (tec) => `<li>
+      <a href="#"> ${tec} </a>
+    </li>`,
+  )}
+    </ul>
+    <button class="btn show-modal" data-id="${index}" type="button">
+      See Project
+    </button>
+  </div>
+  `;
+
+  workOuterContainer.appendChild(workContainer);
+}
+
+// create works section
+workData.forEach((work, index) => {
+  generateWorks(work, index);
+});
+
+// hambuger menu imports
 const btn = document.querySelector('.mb-menu');
 const menu = document.querySelector('.hamburger');
 const menuList = document.querySelectorAll('.mb-menu-list a');
 
+// hambuger menu implementation
 btn.addEventListener('click', () => {
   menu.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
 });
-
-// the pop-up menu
 
 menuList.forEach((list) => {
   list.addEventListener('click', () => {
@@ -15,28 +53,72 @@ menuList.forEach((list) => {
   });
 });
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// modal implementation
+const modalContainer = document.querySelector('.modal-container');
+const workBtns = document.querySelectorAll('.show-modal');
 
-// Get the button that opens the modal
-var bt = document.getElementsByClassName("open");
+// create modal
+function generateModal({
+  ImgUrl, name, description, tech, demoUrl, gitUrl,
+}) {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  modal.innerHTML = `
+    <button type="button" class="modalcross">
+      <span></span>
+      <span></span>
+    </button>
+    <div class="modal-img">
+      <img src="Images/${ImgUrl}" alt="${name}" />
+    </div>
+    <div class="modal-d">
+      <h2 class="modal-title">${name}</h2>
+      <div class="modal-btn desktop">
+        <a href="${demoUrl}" class="btn"
+          ><span>See Live</span> <img src="Images/export.svg" alt="see live"
+        /></a>
+        <a href="${gitUrl}" class="btn"
+          ><span>See Source</span> <img src="Images/github_ic.svg" alt="see live"
+        /></a>
+      </div>
+    </div>
+    <ul class="tags">
+      ${tech.map(
+    (tec) => `<li>
+        <a href="#"> ${tec} </a>
+      </li>`,
+  )}
+    </ul>
+    <p class="modal-desc">
+      ${description}
+    </p>
+    <div class="modal-btn mobile">
+      <a href="${demoUrl}" class="btn"
+        ><span>See Live</span> <img src="Images/export.svg" alt="see live"
+      /></a>
+      <a href="${gitUrl}" class="btn"
+        ><span>See Source</span> <img src="Images/github_ic.svg" alt="see live"
+      /></a>
+    </div>
+  `;
 
-// When the user clicks the button, open the modal 
-bt.onclick = function() {
-  alert("yes ");
+  modalContainer.appendChild(modal);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function closeBtn() {
+  document.querySelector('.modalcross').addEventListener('click', () => {
+    modalContainer.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+workBtns.forEach((workBtn) => {
+  workBtn.addEventListener('click', () => {
+    modalContainer.innerHTML = '';
+    generateModal(workData[workBtn.dataset.id]);
+    closeBtn();
+    modalContainer.classList.add('active');
+    document.body.classList.add('no-scroll');
+  });
+});
