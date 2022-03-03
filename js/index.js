3e9a8137b05e59f25aad7abd2d3f8f2f698cacb1
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-03-03 15:21:33 +0200

    ...

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const workData = [
+    {
+      name: 'Keeping track of hundreds of components 1',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLore",
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/EzekielUtshudi',
+      gitUrl: 'https://github.com/EzekielUtshudi',
+    },
+    {
+      name: 'Keeping track of hundreds of components 2',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+    },
+    {
+      name: 'Keeping track of hundreds of components 3',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+    },
+    {
+      name: 'Keeping track of hundreds of components 4',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+    },
+    {
+      name: 'Keeping track of hundreds of components 5',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: '',
+      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio-Project',
+    },
+    {
+      name: 'Keeping track of hundreds of components 6',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'work.png',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/EzekielUtshudi/Porfolio',
+      gitUrl: 'https://github.com/EzekielUtshudi/Porfolio',
+    },
+  ];
+  
+  export default workData;
