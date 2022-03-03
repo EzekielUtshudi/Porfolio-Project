9c28de4ef7d2929675ead5b796a0c28892ab1144
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-03-03 22:51:42 +0200

    fix js/menu

--- js/menu.js
+++ js/menu.js
@@ -50,7 +50,6 @@ menuList.forEach((list) => {
   list.addEventListener('click', () => {
     menu.classList.remove('active');
     document.body.classList.toggle('no-scroll');
-<<<<<<< HEAD
   });
 });
 
@@ -150,77 +149,3 @@ form.addEventListener('submit', (e) => {
 
   validateForm(form.elements.email_address, MAIL_ERROR);
 });
-=======
-  });
-});
-
-// modal implementation
-const modalContainer = document.querySelector('.modal-container');
-const workBtns = document.querySelectorAll('.show-modal');
-
-// create modal
-function generateModal({
-  ImgUrl, name, description, tech, demoUrl, gitUrl,
-}) {
-  const modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  modal.innerHTML = `
-    <button type="button" class="modalcross">
-      <span></span>
-      <span></span>
-    </button>
-    <div class="modal-img">
-      <img src="Images/${ImgUrl}" alt="${name}" />
-    </div>
-    <div class="modal-d">
-      <h2 class="modal-title">${name}</h2>
-      <div class="modal-btn desktop">
-        <a href="${demoUrl}" class="btn"
-          ><span>See Live</span> <img src="Images/export.svg" alt="see live"
-        /></a>
-        <a href="${gitUrl}" class="btn"
-          ><span>See Source</span> <img src="Images/github_ic.svg" alt="see live"
-        /></a>
-      </div>
-    </div>
-    <ul class="tags">
-      ${tech.map(
-    (tec) => `<li>
-        <a href="#"> ${tec} </a>
-      </li>`,
-  ).join('')}
-    </ul>
-    <p class="modal-desc">
-      ${description}
-    </p>
-    <div class="modal-btn mobile">
-      <a href="${demoUrl}" class="btn"
-        ><span>See Live</span> <img src="Images/export.svg" alt="see live"
-      /></a>
-      <a href="${gitUrl}" class="btn"
-        ><span>See Source</span> <img src="Images/github_ic.svg" alt="see live"
-      /></a>
-    </div>
-  `;
-
-  modalContainer.appendChild(modal);
-}
-
-function closeBtn() {
-  document.querySelector('.modalcross').addEventListener('click', () => {
-    modalContainer.classList.remove('active');
-    document.body.classList.remove('no-scroll');
-  });
-}
-
-workBtns.forEach((workBtn) => {
-  workBtn.addEventListener('click', () => {
-    modalContainer.innerHTML = '';
-    generateModal(workData[workBtn.dataset.id]);
-    closeBtn();
-    modalContainer.classList.add('active');
-    document.body.classList.add('no-scroll');
-  });
-});
->>>>>>> aa21a1622205cf88c9125d0fc1263c55f523e6f9
